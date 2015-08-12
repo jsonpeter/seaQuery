5174505fcbf90bfad78f074714fc3e987004895e
jsonpeter <aspcms@foxmail.com>
2015-08-12 17:08:27 +0800

    2015年8月12日17:08:13
    演示地址增加

--- js/testmain.js
+++ js/testmain.js
@@ -6,25 +6,25 @@ define(function(require, exports, module) {
         $obj.click(function(){
               alert($(this).html());
         })
-       // alert($("#container").attr("data"));
-       // $("#test,.test").html("js wirteout");
-       // $("#container b").html("hellooooooooooo!");
-       //
-       //var obj=$("#container").html("111111");
-       // //obj.hide();
-       //
-       // $obj.html();
-       // $("input").bind("click",function(e){
-       //     alert($(this).find("p").html());
-       //     $(e).stopEventBubble();
-       // })
-       // $("#container").html("<input type='button' value='insert btn'>");
-       // $("input").bind("click",function(e){
-       //     alert($("#container").val());
-       //     $(e).stopEventBubble();
-       // })
-       // $(window).bind("click",function(){
-       //     alert("window");
-       // })
+        alert($("#container").attr("data"));
+        $("#test,.test").html("js wirteout");
+        $("#container b").html("hellooooooooooo!");
+
+       var obj=$("#container").html("111111");
+        //obj.hide();
+
+        $obj.html();
+        $("input").bind("click",function(e){
+            alert($(this).find("p").html());
+            $(e).stopEventBubble();
+        })
+        $("#container").html("<input type='button' value='insert btn'>");
+        $("input").bind("click",function(e){
+            alert($("#container").val());
+            $(e).stopEventBubble();
+        })
+        $(window).bind("click",function(){
+            alert("window");
+        })
     })
 })
